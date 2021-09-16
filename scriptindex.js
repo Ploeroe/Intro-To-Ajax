e6cfbe699c981f85cafc88cc5e0c03d7b4269eb3
Ploeroe <raphael.hakim@student.umn.ac.id>
2021-09-16 23:10:52 +0700

    Ya, namanya juga hidup
    Banyak yang gua ganti, deal with it

--- scriptindex.js
+++ scriptindex.js
@@ -7,7 +7,9 @@ function generateUserTable(data){
         let colName = '<td>' +data[idx].name+'</td>'; //membuat kolom nama
         let colEmail = '<td>' +data[idx].email+'</td>'; //membuat kolom email
         let colCompanyName = '<td>' +data[idx].company.name+'</td>'; //membuat kolom nama perusahaan
-        let btnShowPost = '<td><button class="button-posts" userId='+data[idx].id+' onclick ="loadPosts('+data[idx].id+')">Show Posts</button></td>'; // membuat tombol untuk menampilkan post user
+        let btnShowPost = '<td><button class="button-posts" userId=' +data[idx].id
+                        +' onclick ="loadPosts('+data[idx].id
+                        +')">Show Posts</button></td>'; // membuat tombol untuk menampilkan post user
         // membuat row sejumlah kolom yang ingin ditampilkan
         let newRow = '<tr>'+colID+colName+colEmail+colCompanyName+btnShowPost+'</tr>';
         tbody.innerHTML += newRow; // meng-append row ke dalam tbody
@@ -39,14 +41,17 @@ function onDocumentFinish(){
 }
 function showPosts(data){
     let idx = 0;
-    let div = document.getElementById('user-posts').querySelector('div');
-    div.innerHTML = '';
+    let tbody = document.getElementById('user-posts').querySelector('tbody');
+    tbody.innerHTML = '';
     for(idx = 0; idx < data.length; idx++){ // membuat row dengan kolom yang diinginkan
-        let colTitle = '<h2> Title : '+data[idx].title+'</h2>';
-        let colBody = '<p>...'+data[idx].body+'</p>';
-        let btnShowComments = '<div><button class="button-comments" id="btncomments'+data[idx].id+'" postId='+data[idx].id+' onclick= "loadComments('+data[idx].id+')">Show Comments</button></div>'; // tombol untuk menampilkan comment pada post yang dipilih
-        let newRow = '<div class="card">'+colTitle+colBody+btnShowComments+'</div>';
-        div.innerHTML += newRow; // append row ke div
+        let colID = '<td>'+data[idx].id+'</td>';
+        let colTitle = '<td>'+data[idx].title+'</td>';
+        let colBody = '<td>'+data[idx].body+'</td>';
+        let btnShowComments = '<td><button class="button-comments" postId='+data[idx].id
+        +' on click= "loadComments('+data[idx].id 
+        + ')">Show Comments</button></td>'; // tombol untuk menampilkan comment pada post yang dipilih
+        let newRow = '<tr>'+colID+colTitle+colBody+btnShowComments+'</tr>';
+        tbody.innerHTML += newRow; // append row ke tbody
     }
 }
 function loadPosts(id){
@@ -67,43 +72,4 @@ function loadPosts(id){
         alert('Request Failed! Check your internet connection');
     }
     request.send(); //mengirim request
-}
-function showComments(data){
-    let idx = 0;
-    let x = data[idx].postId;
-    let div = document.getElementById('user-posts').querySelector("[postId='"+x+"']");
-    div.innerHTML = '';
-    for(idx = 0; idx < data.length; idx++){
-        let colName = '<b>'+data[idx].name+'</b>';
-        let colEmail = '('+data[idx].email+')';
-        let colBody = 'commented :"<i>'+data[idx].body+'</i>"';
-        let newRow = '<div class="cards comments">'+colName+' '+colEmail+' '+colBody+'</div>';
-        div.innerHTML += newRow;
-    }
-}
-function loadComments(id){
-    let request = new XMLHttpRequest();
-    let url= 'https://jsonplaceholder.typicode.com/comments/?postId='+id;
-    request.open('GET', url, true);
-
-    request.onload = function(){
-        if(request.status >= 200 && request.status <400){
-            let data = JSON.parse(request.responseText);
-            showComments(data);
-        }
-        else{
-            alert('Page Not Found');
-        }
-    }
-    request.onerror = function(){
-        alert('Request Failed! Check your internet connection');
-    }
-
-    var x = document.getElementById("btncomments"+id);
-	x.style.backgroundColor = "white";
-	x.style.color = "black";
-	x.style.outlineStyle = "none";
-	x.style.cursor = "default";
-
-    request.send();
 }
